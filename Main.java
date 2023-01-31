051246da58740b226f0486de04f442bf668dd0a7
Diego Santos <sandevhacker@gmail.com>
2023-01-31 07:06:30 -0300

    feito com sucesso

---
+++ Main.java
@@ -0,0 +1,52 @@
+
+
+public class Main{
+    public static void main(String [] args) {
+        System.out.println("");
+        System.out.println("Saudação aos vizualizadores");
+        System.out.println("");
+        System.out.println("Conhecendo os tipos de variáveis em java");
+        System.out.println("int, string, double, boolean");
+
+        // "int" recebe o valor de exemplo: = 2"
+
+        int comprimento = 50;
+        int largura = 20;
+        int altura = 20;
+        int perimetro = (altura + comprimento + largura);
+
+            System.out.println("int = "+ perimetro + "cm");
+
+        // escrever aqui sobre a variável string
+
+        String nome = "anderson";
+        String sobrenome_1 = "Pereira";
+        String sobrenome_2 = "Da Silva";
+
+            System.out.println("String = "+ " " + nome + " " + sobrenome_1 + " " + sobrenome_2);
+
+        // escrever aqui sobre a variável double
+
+         double numReal = 7;
+         double numDivisor = 2;
+            System.out.println("Número Real = " + numReal / numDivisor);
+
+
+        // escrever aqui sobre a variável boolean
+
+        boolean trabalharDds = true;
+        boolean trabalharFds = false;
+
+        System.out.println("Eu trabalho Dia de semana = " + trabalharDds);
+        System.out.println("Eu trabalho no final de semana = " + trabalharFds);
+
+
+
+    }
+
+
+
+
+
+    }
+
